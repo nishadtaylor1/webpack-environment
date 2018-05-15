b8840d7efbf8355d6fb77f643e655b0c0f99b65a
wR0Ng <ron.taylor@studiesweekly.com>
2018-05-15 08:00:18 -0600

    the perfect local dev envrionment complete.

---
+++ config/webpack.dev.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const webpack = require("webpack"); 
+const HTMLwebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: {
+    main: [
+      "./src/main.js"
+    ]
+  },
+  mode: "development",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/"
+  },
+
+  devServer: {
+    contentBase: "dist",
+    overlay: true,
+    hot: true,
+    stats: {
+      colors: true
+    }
+  },
+
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          }
+
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              attrs: ["img:src"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|gif|png)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLwebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+}
